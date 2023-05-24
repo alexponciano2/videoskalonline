aa5cfa4bf4f6c2e6882c7517934299033ee8a398
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-05-24 19:43:43 -0300

    Created component folder
    Also added Header and Footer components

---
+++ kalflix/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <span>KalFlix &copy; Desenvolvido por Alex Ponciano - 2023</span>
+        </footer>
+    );
+}
+
+export default Footer;
