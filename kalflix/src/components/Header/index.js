a37bc2b279d53ed0fc2badb4ed83e027b9b16906
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-05-25 19:12:25 -0300

    added some effects

--- kalflix/src/components/Header/index.js
+++ kalflix/src/components/Header/index.js
@@ -5,8 +5,8 @@ function Header() {
         <header className={styles.header}>
             <span>KalFlix</span>
             <nav>
-                <a href="#">Home</a>
-                <a href="#">Contact</a>
+                <a href="https://github.com/alexponciano2" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/alex-ponciano/">LinkedIn</a>
             </nav>
         </header>
     );
